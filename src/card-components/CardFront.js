26795c4374cbafcc098e5b1797ba271097f01829
kaidornel <kai_dornel@hotmail.com>
2020-05-10 16:40:27 -0400

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -4,8 +4,21 @@ export default class CardFront extends Component {
 
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
     )
   }
 }
+
+// import React, { Component } from 'react';
+
+// export default class CardFront extends Component {
+// 	render() {
+// 		return (
+// 			<div
+// 				className="card-front"
+// 				style={{ backgroundImage: `url(${this.props.poster})` }}
+// 			/>
+// 		);
+// 	}
+// }
