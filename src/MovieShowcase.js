1eb739945cc1bd68a7db3303e103a4b428df0bfc
Dawit Gizaw <gizawdawit123.com>
2020-05-12 00:35:04 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
+import { MultiCompiler } from 'webpack';
 
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(movie => <MovieCard IMDBRating= {movie.IMDBRating} poster={movie.poster} title={movie.title} genres={movie.genres} />)
   }
 
   render() {
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+
 export default class CardFront extends Component {
 
   render() {
+    console.log(this.props.style)
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{ backgroundImage: `url(${this.props.poster})` }}>
+        
       </div>
     )
   }
